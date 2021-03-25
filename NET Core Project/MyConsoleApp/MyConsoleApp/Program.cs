10ec23fc662bd5255947c905bc678d469c3b18bd
NationsAnarchy <73151692+NationsAnarchy@users.noreply.github.com>
2021-03-25 23:28:05 +0700

    Testing new Git repo by my own.

---
+++ NET Core Project/MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing Git repo on my own.");
+        }
+    }
+}
