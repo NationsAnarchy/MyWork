a76a36b73e18649c6f4cffe95124ebe1de36f90e
NationsAnarchy <73151692+NationsAnarchy@users.noreply.github.com>
2021-03-27 21:35:50 +0700

    Working around with C# console app

--- NET Core Project/MyConsoleApp/MyConsoleApp/Program.cs
+++ NET Core Project/MyConsoleApp/MyConsoleApp/Program.cs
@@ -8,6 +8,29 @@ namespace MyConsoleApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Testing Git repo on my own.");
+            Console.WriteLine();
+            
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine("Looping for fun xDD");
+            }
+
+            Console.Write("Nhập vào số a: ");
+            decimal a = decimal.Parse(Console.ReadLine() ?? string.Empty);
+            Console.Write("Nhập vào số b: ");
+            decimal b = decimal.Parse(Console.ReadLine() ?? string.Empty);
+
+            decimal tong = a + b;
+            decimal hieu = a - b;
+            decimal tich = a * b;
+            decimal thuong = a / b;
+            decimal du = a % b;
+            
+            Console.WriteLine($"Phép cộng a và b là: {tong}");
+            Console.WriteLine($"Phép trừ a và b là: {hieu}");
+            Console.WriteLine($"Phép nhân a và b là: {tich}");
+            Console.WriteLine($"Phép chia a và b là: {decimal.Round(thuong, 3)}");
+            Console.WriteLine($"Phép lấy dư a và b là: {du}");
         }
     }
 }
